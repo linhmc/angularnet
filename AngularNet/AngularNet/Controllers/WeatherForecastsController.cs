90116b14313308d8952162a5a0de8f5c5e034dbf
linhmc <linhmc.vn@gmail.com>
2018-03-22 23:26:07 +0700

    Generate DataContext

---
+++ AngularNet/AngularNet/Controllers/WeatherForecastsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AngularNet.Models;
+
+namespace AngularNet.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/WeatherForecasts")]
+    public class WeatherForecastsController : Controller
+    {
+        private readonly SampleDataContext _context;
+
+        public WeatherForecastsController(SampleDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/WeatherForecasts
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetWeatherForecast()
+        {
+            return _context.WeatherForecast;
+        }
+
+        // GET: api/WeatherForecasts/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetWeatherForecast([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var weatherForecast = await _context.WeatherForecast.SingleOrDefaultAsync(m => m.DateFormatted == id);
+
+            if (weatherForecast == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(weatherForecast);
+        }
+
+        // PUT: api/WeatherForecasts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWeatherForecast([FromRoute] string id, [FromBody] WeatherForecast weatherForecast)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != weatherForecast.DateFormatted)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(weatherForecast).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WeatherForecastExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/WeatherForecasts
+        [HttpPost]
+        public async Task<IActionResult> PostWeatherForecast([FromBody] WeatherForecast weatherForecast)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.WeatherForecast.Add(weatherForecast);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (WeatherForecastExists(weatherForecast.DateFormatted))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetWeatherForecast", new { id = weatherForecast.DateFormatted }, weatherForecast);
+        }
+
+        // DELETE: api/WeatherForecasts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWeatherForecast([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var weatherForecast = await _context.WeatherForecast.SingleOrDefaultAsync(m => m.DateFormatted == id);
+            if (weatherForecast == null)
+            {
+                return NotFound();
+            }
+
+            _context.WeatherForecast.Remove(weatherForecast);
+            await _context.SaveChangesAsync();
+
+            return Ok(weatherForecast);
+        }
+
+        private bool WeatherForecastExists(string id)
+        {
+            return _context.WeatherForecast.Any(e => e.DateFormatted == id);
+        }
+    }
+}
---
+++ AngularNet/AngularNet/Models/SampleDataContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AngularNet.Models
+{
+    public partial class SampleDataContext : DbContext
+    {
+        public virtual DbSet<WeatherForecast> WeatherForecast { get; set; }
+
+        public SampleDataContext(DbContextOptions<SampleDataContext> options): base(options)
+        {
+
+        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\SqlLocal2014;Initial Catalog=SampleData;Integrated Security=True;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WeatherForecast>(entity =>
+            {
+                entity.HasKey(e => e.DateFormatted);
+
+                entity.Property(e => e.DateFormatted)
+                    .HasColumnName("dateFormatted")
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Summary)
+                    .HasColumnName("summary")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TemperatureC).HasColumnName("temperatureC");
+
+                entity.Property(e => e.TemperatureF).HasColumnName("temperatureF");
+            });
+        }
+    }
+}
